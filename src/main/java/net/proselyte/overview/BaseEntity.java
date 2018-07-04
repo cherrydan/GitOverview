af898965a6ffa22aaa807c454a0e7e395572e3d6
danny <cherrydan.1978@gmail.com>
2018-07-04 13:01:50 +0300

    Adding new class BaseEntity

---
+++ src/main/java/net/proselyte/overview/BaseEntity.java
@@ -0,0 +1,27 @@
+package net.proselyte.overview;
+
+public class BaseEntity {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "BaseEntity [id=" + id + "]";
+	}
+
+	public Boolean isNew() {
+		return (this.id == null);
+
+	}
+
+
+
+}
