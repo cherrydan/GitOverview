4883c2192642acd1768e800eeb508eb7c2850476
Unknown <cherrydan.1978@gmail.com>
2018-07-04 13:29:24 +0300

    Add new class to PersonClassBranch

---
+++ src/main/java/net/proselyte/overview/Person.java
@@ -0,0 +1,33 @@
+package net.proselyte.overview;
+
+public class Person extends BaseEntity {
+
+	private String firstName;
+
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", id = " + super.getId() + " lastName=" + lastName + "]";
+	}
+
+
+
+
+}
